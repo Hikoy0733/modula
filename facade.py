1d91b91a76efbb69bcceb792ea9c97c0cd97b752
Hikoy0733 <clemru4@gmail.com>
2024-11-29 08:34:08 +0100

    Add files via upload

--- facade.py
+++ facade.py
@@ -1,5 +1,4 @@
 import turtle
-import couleur_aleatoire
 from rectangle import rectangle
 
 def facade(x, y_sol, couleur, niveau):
@@ -12,15 +11,13 @@ def facade(x, y_sol, couleur, niveau):
     remarque :
         Facade dessine une facade sans les élements interieurs
     '''
-
-    turtle.fillcolor(couleur)  #Change la couleur de remplissage à rouge
-    turtle.begin_fill()  #Précise le début du remplissage
+    turtle.fillcolor(couleur)
+    turtle.begin_fill()
     rectangle(x,y_sol + 60 * niveau,140,60)
     turtle.end_fill()
+    pass
 
 if __name__ == '__main__':
-
-    facade(0,0,couleur_aleatoire.couleur_aleatoire(),0)
-
+    facade(0,0,"red",0)
     # On ferme la fenêtre s'il y a un clique gauche
     turtle.exitonclick()
